c0521548c371ba8488c3570ef460e52d47d479b9
Hafidz <hnpradigda@gmail.com>
2025-05-15 00:48:13 +0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -128,8 +128,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
+    "https://my-portfolio-hafidz-backend.up.railway.app",
+    "https://my-portfolio-hafidz.vercel.app"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://my-portfolio-hafidz-backend.up.railway.app",
     "https://my-portfolio-hafidz.vercel.app"
 ]
 
