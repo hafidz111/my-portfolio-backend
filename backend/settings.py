d59e1f28d9c11a503e4e24d41e4566896c9f2316
Hafidz <hnpradigda@gmail.com>
2025-05-14 23:54:20 +0700

    settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -130,8 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    #"https://my-portfolio-hafidz.vercel.app",
-    "http://localhost:3000"
+    "https://my-portfolio-hafidz.vercel.app"
 ]
 
 REST_FRAMEWORK = {
